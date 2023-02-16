6c2ba843a24601e301008e3b9a2487eb88eca722
Bryan Costa <bryancmoraes776@gmail.com>
2023-02-15 23:25:21 -0300

    insertionSort

--- index.js
+++ index.js
@@ -6,38 +6,40 @@ app.use(express.json())
 
 app.get('/server', (req, res) => {
     
-    let yer = [10, 5, 9, 7, 10, 1, 2, 5, 4]
+    let yer = [3, 5, 1, 2, 4]
     
-    function bubbleSort(arr){
-        swapped = false;
-        
-        let end = arr.length - 1
-
-        let temp;
-    
-        for(i = 0; i < end; i++){
-            for(j = 0; j < arr[i]; j++){
-                if(arr[i] > arr[i + 1]){
-                    swapped = true
-                    temp = arr[i]
-                    arr[i] = arr[i + 1]
-                    arr[i + 1] = temp
+    function insertionSort(arr){
+        let valor_Atual
+        let j;
+        for(let i = 1; i < arr.length; i++){
+            valor_Atual = arr[i];
+            j = i - 1;
+                while(j >= 0 && arr[j] > valor_Atual){
+                    arr[j + 1] = arr[j];
+                    j--;
                 }
-                
+                arr[j+1] = valor_Atual            
             }
+            return arr
         }
-        
-    }
-    do{
-        bubbleSort(yer)
-    }while(swapped)
 
-    res.json(yer)
-
-
-})
+        let result = insertionSort(yer)
+        res.json(result)
+    })
 
 
 app.listen(8282, () => {
     console.log('vai rolar o bagulho doido!')
 })
+
+
+/* let index_min;
+            index_min = i;
+            for(let j = i + 1; j < arr.length; j++){
+                if(arr[j] < arr[index_min]){
+                    index_min = j
+
+                temps = arr[i]
+                arr[i] = arr[index_min]
+                arr[index_min] = temps
+                } */
